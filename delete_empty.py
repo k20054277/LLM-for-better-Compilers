a291f61ecacb5f30eeccd46beba53df5af8e5899
Jiaqi Xu <k20054277@kcl.ac.uk>
2024-04-24 01:40:03 +0100

    update delete_empty

--- delete_empty.py
+++ delete_empty.py
@@ -1,31 +1,25 @@
 import os 
 import glob 
+import sys
   
-  
-def delete_empty_files_using_glob(pathname: str): 
+def delete_empty_files_using_glob(path): 
 
-    no_of_files_deleted = 0
     total_size = 0
-  
-    # Get the path that are matching with the pattern 
-    files = glob.glob(pathname) 
-  
-    for path in files: 
-        size = os.path.getsize(path)
-        # Check if the path is a file and empty (size = 0) 
-        if os.path.isfile(path):
-            if size == 0: 
-                # Print the path of the file that will be deleted 
-                print("Deleting File >>>", path.replace('\\', '/')) 
+    no_of_files_deleted = 0
+
+    for entry in os.scandir(path):
+        if entry.is_file():
+            size = entry.stat().st_size
+            if size == 0:
+                print("Deleting File >>>", entry.path.replace('\\', '/')) 
                 # Delete the empty file 
-                os.remove(path) 
-      
+                os.unlink(entry.path) 
                 no_of_files_deleted += 1
             else:
-                total_size += size
-    
-    return no_of_files_deleted, total_size
-  
+                total_size += entry.stat().st_size
+    return total_size, no_of_files_deleted
+
+
 def read_folder(model):
 
     if model == "Mistral": 
@@ -45,30 +39,31 @@ def read_folder(model):
         model = input("Please select a model to check LLM generation statistics and remove anomalies: ")
         read_folder(model)
 
-    if '.DS_Store' in dir_path:
-        dir_path.remove('.DS_Store')
+    workdir=os.listdir(dir_path)
+    if '.DS_Store' in workdir:
+        workdir.remove('.DS_Store')
     total = len(os.listdir(dir_path))
     print("-------------------------------------")
     if total == 0:
         print(f"No files were found in the folder belonging to {model}, check files are correctly stored under {dir_path}!")
         sys.exit() 
     else:
-        empty, total_size = delete_empty_files_using_glob(dir_path)
-        gen = str(round((100 * float(empty)/float(total_size)),2)) + "%"
+        total_size, empty = delete_empty_files_using_glob(dir_path)
+        gen = str(round(100 * (float(total)-float(empty))/float(total),2)) + "%"
         mean = round(total_size/(total-empty), 2)
         file = open(model+"_statistics", 'a')
-        file.write(f"{model} generation statistics: ")
-        file.write(f"{str(empty)} anomalies were found out of {str(total)} programs")
-        file.write(f"Percentage of succesfull generation: {gen}")
-        file.write(f"Mean size of succesfull generations: {mean} Bytes")
+        file.write(f"{model} generation statistics: \n")
+        file.write(f"{str(empty)} anomalies were found out of {str(total)} programs\n")
+        file.write(f"Percentage of successful generation: {gen}\n")
+        file.write(f"Mean size of successful generation: {mean} Bytes\n\n")
 
-        file1.close()
+        file.close()
         if empty == 0:
             print(f"No anomalies found, LLM generation statistics stored in {model}_statistics.txt")
         else:
             print("-------------------------------------")
             print(f"{empty} anomalies found, LLM generation statistics stored in {model}_statistics.txt")
-        
+        sys.exit()
 
 def main():
     print("Large Language Model options: ")
